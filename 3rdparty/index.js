9592d717aa3c141bef3b7d7e4ec57b0598dde976
azu <azuciao@gmail.com>
2023-07-17 14:52:41 +0900

    3rd

--- 3rdparty/index.js
+++ 3rdparty/index.js
@@ -1,8 +1,1 @@
-console.log("load entry");
-console.log("load 3rdparty-for-web for each website");
-try {
-    await import("3rdparty-for-web")
-    console.log("load 3rdparty-for-web for each website success");
-} catch (e) {
-    console.error("load 3rdparty-for-web for each website failed");
-}
+import　"3rdparty-for-web"
